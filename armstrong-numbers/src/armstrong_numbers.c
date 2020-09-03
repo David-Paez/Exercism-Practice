47077991f58d5b029c291e507bcd11851cbfa702
David Paez <dpaez013@fiu.edu>
2020-09-03 16:42:50 -0400

    Refactored armstrong_numbers.c

--- armstrong-numbers/src/armstrong_numbers.c
+++ armstrong-numbers/src/armstrong_numbers.c
@@ -5,43 +5,29 @@
 int count(int num)
 {
     int count = 0;
+
     while(num > 0)
     {
         num /= 10;
         count++;
     }
+
     return count;
 }
 
 bool is_armstrong_number(int candidate)
 {
-    int length = count(candidate);
+    int digits = count(candidate);
     int num = candidate;
     int i = 0;
     int sum = 0;
-    int arr[length];
-    bool isArmstrong = false;
 
-    if (candidate == 0)
+    for(i = 0; i < digits; i++)
     {
-        return true;
-    }
-
-    for(i = 0; i < length; i++)
-    {
-        arr[i] = num % 10;
+        sum += pow(num%10, digits);
         num /= 10;
     }
 
-    for(i = 0; i < length; i++)
-    {
-      sum += pow(arr[i], length);
-    }
-
-    if(sum == candidate)
-    {
-        isArmstrong = true;
-    }
-
-    return isArmstrong;
+    if(sum == candidate) return true;
+    else return false;
 }
